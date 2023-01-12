850115f5929367fc5516be1992997e698618f4ba
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-01-12 14:11:46 -0800

    move to computer

--- python/test_code.py
+++ python/test_code.py
@@ -1,8 +1,20 @@
-x = 3
-print(x) # 3
-def update_x():
-    return 6
-x = update_x()
-print(x) # 6
+# README: To test out code during Code bootcamp
 
+# List of dictionaries
+users = [
+    {"first": "Ada", "last": "Lovelace"}, # index 0
+    {"first": "Alan", "last": "Turing"}, # index 1
+    {"first": "Eric", "last": "Idle"} # index 2
+]
+# Dictionary of lists
+resume_data = {
+    #        	     0           1           2
+    "skills": ["front-end", "back-end", "database"],
+    #                0           1
+    "languages": ["Python", "JavaScript"],
+    #                0              1
+    "hobbies":["rock climbing", "knitting"]
+}
 
+print(resume_data["skills"][1]) # back-end
+print(users[2]["first"]) # Eric
