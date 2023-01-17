903efcccdb798f8ce69e798b54f4c2c2028f6409
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-01-17 14:16:56 -0800

    move to computer

--- python/flask/fundamentals/assignment_playground/server.py
+++ python/flask/fundamentals/assignment_playground/server.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route("/play")
 def play():
-    return render_template("play.html")
+    return render_template("bonus.html", repeat = 3, color = "skyblue")
 
 @app.route("/play/<int:repeat>")
 def play_repeat(repeat):
-    return render_template("play_repeat.html", repeat = repeat)
+    return render_template("bonus.html", repeat = repeat, color = "skyblue")
 
 @app.route("/play/<int:repeat>/<color>")
 def play_repeat_color(repeat, color):
-    return render_template("play_repeat_color.html", repeat = repeat, color = color)
+    return render_template("bonus.html", repeat = repeat, color = color)
 
 if __name__ == "__main__":
     app.run(debug = True)
