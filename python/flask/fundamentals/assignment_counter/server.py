e580f47c2a45f2666bd36a0cbacf14477080d594
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-01-18 12:06:59 -0800

    test modify

--- python/flask/fundamentals/assignment_counter/server.py
+++ python/flask/fundamentals/assignment_counter/server.py
@@ -14,6 +14,31 @@ def index():
 @app.route("/destroy_session")
 def destroy_session():
     session.clear()
+    print("Session cleared")
+    return redirect("/")
+
+@app.route("/2")
+def add_two():
+    session["count"] += 1
+    return redirect("/")
+
+@app.route("/increment_by")
+def increment_by():
+    print(session.get("increment_by"))
+    current = session["count"]
+    if "increment_by" in session:
+        increment = int(session["increment_by"])
+        current = int(current)
+        current = current + increment - 1
+        session["count"] = current
+        return redirect("/")
+    else:
+        current = int(current)
+        current -= 1
+        session["count"] = current
+        return redirect("/")
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
