88c2818bd5d4365b96621e639bc41e29ef171e78
Anthony Saephan <74607823+AnthonyRSaephan@users.noreply.github.com>
2023-01-24 06:55:14 -0800

    move to laptop

---
+++ python/flask_mysql/crud/dojos_and_ninjas/server.py
@@ -0,0 +1,3 @@
+from flask_app import app
+from flask_app.controllers import dojo_controller, ninja_controller
+DATABASE = "dojos_and_ninjas"
--- python/test_code.py
+++
@@ -1,9 +0,0 @@
-# README: To test out any code during Code bootcamp
-x = 5
-a = f"""
-text{x}
-text
-text
-"""
-
-print(a)
