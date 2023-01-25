bf97a84da0b0588c841b0495ce68a214aaa437da
Anthony Saephan <74607823+AnthonyRSaephan@users.noreply.github.com>
2023-01-25 04:47:03 -0800

    emphasized items

---
+++ python/flask_mysql/validation/assignment_login_and_registration/flask_app/controllers/users_controller.py
@@ -0,0 +1,25 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models import user_model
+
+@app.route('/')
+@app.route('/login_register')
+def login_register():
+    session.clear()
+    return render_template('login_register.html')
+
+@app.route('/users/login', methods=['POST'])
+def users_login():
+    pass
+
+@app.route('/users/register', methods=['POST'])
+def users_register():
+    is_valid = user_model.User.validate_user(request.form)
+    if not is_valid:
+        return redirect("/login_register")
+    user_model.User.create(request.form)
+    session["first_name"] = request.form["first_name"]
+    session["last_name"] = request.form["last_name"]
+    session["email"] = request.form["email"]
+    session["id"] = request.form["id"]
+    return render_template("/dashboard.html")
---
+++ python/flask_mysql/validation/assignment_login_and_registration/flask_app/models/user_model.py
@@ -0,0 +1,124 @@
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE
+import re
+import sys
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User():
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.updated_at = data['updated_at']
+        self.created_at = data['created_at']
+
+    @classmethod
+    def create(cls, data):
+        query = """
+            INSERT INTO users (first_name, last_name, email, password)
+            VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);
+        """
+
+        return connectToMySQL(DATABASE).query_db(query, data)
+
+    
+    @classmethod
+    def get_all(cls):
+        pass
+
+    @classmethod
+    def update(cls, data):
+        query = """
+            UPDATE users
+            SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, password = %(password)s
+            WHERE id = %(id)s;
+        """
+
+        data = {
+            'id': data['id'], 
+            'first_name': data['first_name'],
+            'last_name': data['last_name'],
+            'email': data['email'],
+            'password': data['password']
+        }
+
+        connectToMySQL(DATABASE).query_db(query, data)
+
+    @classmethod
+    def delete(cls, id):
+        query = """
+            DELETE FROM users
+            WHERE id = %(id)s;
+        """
+
+        data = {
+            "id" : id
+        }
+
+        connectToMySQL(DATABASE).query_db(query, data)
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        query = """
+            SELECT * FROM users
+            WHERE email = %(email)s;
+        """
+        data = {
+            "email" : email
+        }
+
+        results = connectToMySQL(DATABASE).query_db(query, data)
+        if results:
+            for row in results:
+                return cls(row)
+
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+        if data["first_name"] == "":
+            flash("First name is required.")
+            is_valid = False
+        elif not data["first_name"].isalpha():
+            flash("First name alphanumeric. only letters, numbers")
+            is_valid = False
+        elif len(data["first_name"]) < 2:
+            flash("First name must be at least 2 characters.")
+            is_valid = False
+        elif len(data["first_name"]) > 255:
+            flash("First name must be less than 255 characters.")
+            is_valid = False
+        
+        if data["last_name"] == "":
+            flash("Last name is required.")
+            is_valid = False
+        elif not data["last_name"].isalpha():
+            flash("Last name alphanumeric. only letters, numbers")
+            is_valid = False
+        elif len(data["last_name"]) < 2:
+            flash("Last name must be at least 2 characters.")
+            is_valid = False
+        elif len(data["last_name"]) > 255:
+            flash("Last name must be less than 255 characters.")
+            is_valid = False
+        
+        if not EMAIL_REGEX.match(data["email"]):
+            flash("Email must be a valid email address.")
+            is_valid = False
+        if User.get_user_by_email(data["email"]):
+            flash("Email already exists.")
+            is_valid = False
+        if len(data["password"]) < 8:
+            flash("Password must be at least 8 characters.")
+            is_valid = False
+        if data["password"] != data["confirm_password"]:
+            flash("Passwords do not match.")
+            is_valid = False
+        flash("test")
+        return is_valid
+
+
