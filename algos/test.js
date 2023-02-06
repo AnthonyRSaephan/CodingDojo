b51261cd7a82b95b6a0e253e8a212dcf1bc15c23
Anthony Saephan <74607823+AnthonyRSaephan@users.noreply.github.com>
2023-02-06 07:16:55 -0800

    move to laptop

--- algos/test.js
+++ algos/test.js
@@ -1,38 +1,12 @@
-const nums1 = [-2, 5, 7, 0, 3];
-const expected1 = 2;
+const beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+    function actuallyPrintingNames() {
+        for (let index = 0; index < names.length; index++) {
+            const name = names[index];
 
-const nums2 = [9, 9];
-const expected2 = -1;
-
-const nums3 = [3,0,1,1,1]
-const expected3 = 1
-
-
-function balanceIndex(nums) {
-    var leftSum = 0
-    var rightSum = 0
-
-    if (nums.length < 3) {
-        return -1
-    } else {
-        for (var i = 0; i < nums.length; i++) {
-            for (var x = 0; x <= i; x++) {
-                leftSum += nums[x]
-            }
-            for (var j = nums.length - 1; j >= i; j--) {
-                rightSum += nums[j]
-            }
-            if (leftSum != rightSum) {
-                leftSum = 0
-                rightSum = 0
-            }
-            else {
-                return i
-            }
+            console.log(name + ' was found at index ' + index);
         }
     }
+    actuallyPrintingNames();
 }
-
-console.log(balanceIndex(nums1))
-console.log(balanceIndex(nums2))
-console.log(balanceIndex(nums3))
+printNames(beatles);
