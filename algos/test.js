90e2c0905fdd8eb9e1ec9e8752c1127e36c1d92d
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-02-06 11:36:26 -0800

    completed hoist

--- algos/test.js
+++ algos/test.js
@@ -1,12 +1,7 @@
-const beatles = ['Paul', 'George', 'John', 'Ringo'];
-function printNames(names) {
-    function actuallyPrintingNames() {
-        for (let index = 0; index < names.length; index++) {
-            const name = names[index];
-
-            console.log(name + ' was found at index ' + index);
-        }
-    }
-    actuallyPrintingNames();
+magicalUnicorns();
+var magicalUnicorns = function(){
+    console.log("Will it blend?");
 }
-printNames(beatles);
+console.log("Don't breathe this!");
+
+
