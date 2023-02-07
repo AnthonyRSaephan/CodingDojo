54d6bfd8b8e096a7fdebb823cdb32f2fd0d20049
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-02-07 15:38:09 -0800

    move to computer

--- algos/test.js
+++ algos/test.js
@@ -1,38 +1,7 @@
-class Deck {
-    constructor() {
-        const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
-        const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-        const deck = [];
-        for (const suit of suits) {
-            for (const face of faces) {
-                deck.push(this.createCard(suit, face));
-            }
-        }
-        this.deck = deck;
-    }
-}
+setTimeout(function () {
+    console.log("start")
+}, 3000);
 
-class Deck {
-    constructor() {
-        const suits = ['Diamond', 'Heart', 'Spade', 'Club'];
-        const faces = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-        const deck = [];
-        suits.forEach(function (suit) {
-            faces.forEach(function (face) {
-                deck.push(this.createCard(suit, face));
-            });
-        });
-        this.deck = deck;
-    }
-}
+console.log("end");
 
-class Deck {
-    constructor() {
-        suits.forEach(suit => {
-            faces.forEach(face => {
-                deck.push(this.createCard(suit, face));
-            });
-        });
-    }
-}
 
