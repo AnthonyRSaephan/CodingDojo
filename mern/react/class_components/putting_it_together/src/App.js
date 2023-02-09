4f81d38e83c69ac9e4689f3a710c3c5a1c05e101
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-02-09 12:17:51 -0800

    completed putting_it_together

--- mern/react/class_components/putting_it_together/src/App.js
+++ mern/react/class_components/putting_it_together/src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function def(){
-  alert("test")
-}
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
-    <button onClick={def}>Click Me</button>
+    <div className="App">
+      <PersonCard firstName = "Jane" lastName = "Doe" age = {45} hairColor = "Black" />
+      <PersonCard firstName = "John" lastName = "Smith" age = {88} hairColor = "Brown" />
+      <PersonCard firstName = "Millard" lastName = "Fillmore" age = {50} hairColor = "Brown" />
+      <PersonCard firstName = "Maria" lastName = "Smith" age = {62} hairColor = "Brown" />
+    </div>
   );
 }
 
 export default App;
-
-
---
+++ mern/react/class_components/putting_it_together/src/components/PersonCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export class PersonCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            age : props.age
+        }
+    }
+
+    
+
+    render() {
+        const {firstName, lastName, hairColor} = this.props
+
+        const incrementBirthday = () => {
+            // console.log(`Birthday is now ${this.state.age}`) oops this didn't console as expected. Forgot to not use this
+            this.setState({
+                age: this.state.age + 1
+            })
+        }
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: hairColor</p>
+                <button onClick={incrementBirthday}>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard
