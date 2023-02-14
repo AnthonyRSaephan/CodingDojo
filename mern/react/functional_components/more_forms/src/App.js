406a1694ce08f0d4d1d6eb6a2a8cfddecf5624d4
Anthony Saephan <74607823+AnthonyRSaephan@users.noreply.github.com>
2023-02-13 23:28:40 -0800

    completed sensei bonus for more_forms

--- mern/react/functional_components/more_forms/src/App.js
+++ mern/react/functional_components/more_forms/src/App.js
@@ -15,27 +15,27 @@ function App() {
       <div className={style.input}>
         <label htmlFor='firstName'>First Name: </label>
         <input id='firstName' onChange={e => setFirstName(e.target.value)}></input>
-        <p>{firstName.length < 2 && "First Name must be at least 2 characters"}</p>
+        <p>{firstName.length > 0 && firstName.length < 2 && "First Name must be at least 2 characters"}</p>
         <br/>
       </div>
 
       <div className={style.input}>
         <label htmlFor='lastName'>Last Name: </label>
         <input id='lastName' onChange={e => setLastName(e.target.value)}></input>
-        <p>{lastName.length < 2 && "Last Name must be at least 2 characters"}</p>
+        <p>{lastName.length > 0 && lastName.length < 2 && "Last Name must be at least 2 characters"}</p>
       </div>
 
       <div className={style.input}>
         <label htmlFor='email'>Email: </label>
         <input id='email' onChange={e => setEmail(e.target.value)}></input>
-        <p>{email.length < 2 && "Email must be at least 2 characters"}</p>
+        <p>{email.length > 0 && email.length < 2 && "Email must be at least 2 characters"}</p>
       </div>
 
       <div className={style.input}>
         <label htmlFor='password'>Password: </label>
         <input id='password' onChange={e => setPassword(e.target.value)}></input>
-        <p>{password.length < 8 && "Password must be at least 8 characters"}</p>
-        <p>{password != confirmPassword && "Passwords must match"}</p>
+        <p>{password.length > 0 && password.length < 8 && "Password must be at least 8 characters"}</p>
+        <p>{password.length > 0 && password != confirmPassword && "Passwords must match"}</p>
       </div>
       <div className={style.input}>
         <label htmlFor='confirmPassword'>Confirm Password: </label>
