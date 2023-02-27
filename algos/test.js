760abea65aef0e824463dd3a1bfea680e981bcd3
Anthony Saephan <74607823+AnthonyRSaephan@users.noreply.github.com>
2023-02-27 06:59:04 -0800

    move to laptop

---
+++ algos/test.js
@@ -0,0 +1,14 @@
+{
+    name: "Pierre Gasly",
+    home_state: "France",
+    lucky_number: 10,
+    birthday: {
+        month: 2,
+        day: 7,
+        year: 1996
+    }
+}
+
+[
+    "coding", "brunch", "MongoDB"
+]
