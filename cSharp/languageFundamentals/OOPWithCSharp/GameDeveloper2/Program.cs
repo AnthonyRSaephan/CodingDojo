91a20680b4116d1aa51e2a6e3e655509a396680c
Anthony Saephan <anthonyrsaephan@gmail.com>
2023-03-10 11:21:50 -0800

    Completed Game Developer 2

--- cSharp/languageFundamentals/OOPWithCSharp/GameDeveloper2/Program.cs
+++ cSharp/languageFundamentals/OOPWithCSharp/GameDeveloper2/Program.cs
@@ -1,8 +1,3 @@
-﻿Enemy bowser = new Enemy("Bowser");
-Attack fireBreath = new Attack("Fire Breath", 10);
-Attack tailSwipe = new Attack("Tail Swipe", 5);
-Attack headSmash = new Attack("Head Smash", 20);
-bowser.addAttack(fireBreath);
-bowser.addAttack(tailSwipe);
-bowser.addAttack(headSmash);
-bowser.randomAttack();
+MeleeFighter warrior = new MeleeFighter("Warrior");
+RangedFighter archer = new MeleeFighter("Archer");
+MagicCaster mage = new MagicCaster("Mage");
